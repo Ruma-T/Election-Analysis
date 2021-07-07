bda072eebb33fb434881b4819882918ef4484d0b
Ruma Talukder <sreeruta@yahoo.com>
2021-07-07 07:49:32 -0500

    Adding the election analysis

--- PyPoll.py
+++ PyPoll.py
@@ -3,6 +3,7 @@ import csv
 import os
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+print(file_to_load)
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Initialize a total vote counter.
@@ -87,4 +88,4 @@ with open(file_to_load) as election_data:
 
     # Read and print the header row.
     headers = next(file_reader)
-    #print(headers)
+    print(headers)
